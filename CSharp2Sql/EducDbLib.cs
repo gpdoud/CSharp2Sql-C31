6a6a67b0208b514ebded9c4e0079d18ad4ed8c65
Greg Doud <gpdoud@gmail.com>
2021-02-22 16:52:18 -0500

    Add project files.

---
+++ CSharp2Sql/EducDbLib.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+
+using System;
+
+namespace CSharp2Sql {
+
+    public class EducDbLib {
+
+        public SqlConnection connection { get; set; }
+
+        public void ExecSelect() {
+            var sql = "SELECT * From Student;";
+            var cmd = new SqlCommand(sql, connection);
+            var reader = cmd.ExecuteReader();
+            while(reader.Read()) {
+                var id = Convert.ToInt32(reader["Id"]);
+                var lastname = reader["Lastname"].ToString();
+                Console.WriteLine($"id={id}, lastname={lastname}");
+            }
+            reader.Close();
+        }
+
+        public void Connect(string database) {
+            var connStr = $"server=localhost\\sqlexpress;" +
+                            $"database={database};" +
+                            "trusted_connection=true;";
+            connection = new SqlConnection(connStr);
+            connection.Open();
+            if(connection.State != System.Data.ConnectionState.Open) {
+                throw new Exception("Connection is not open!");
+            }
+        }
+        public void Disconnect() {
+            connection.Close();
+        }
+    }
+}
---
+++ TestCShart2Sql/Program.cs
@@ -0,0 +1,16 @@
+﻿using CSharp2Sql;
+
+using System;
+
+namespace TestCShart2Sql {
+    class Program {
+        static void Main(string[] args) {
+
+            var sql = new EducDbLib();
+            sql.Connect("EdDb");
+            Console.WriteLine("Connected successfully!");
+            sql.ExecSelect();
+            sql.Disconnect();
+        }
+    }
+}
